c57b1b71aa10d62250395bb654f423a47100cea2
Joshua Min <minje@usc.edu>
2019-03-25 13:23:48 -0700

    Fixed restaurant servlet. Was not returning JSON before.

--- backend/src/main/java/ImHungryServlet/RestaurantServlet.java
+++ backend/src/main/java/ImHungryServlet/RestaurantServlet.java
@@ -8,45 +8,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  * Servlet implementation class RestaurantServlet
  */
 @WebServlet("/RestaurantServlet")
 public class RestaurantServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-  /**
-   * Default constructor. 
-   */
-  public RestaurantServlet() {
-    super();
-    // TODO Auto-generated constructor stub
-  }
+        /**
+         * Default constructor. 
+         */
+        public RestaurantServlet() {
+                super();
+                // TODO Auto-generated constructor stub
+        }
 
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-   */
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // TODO Auto-generated method stub
-		
-    // Get queries
-    String term = request.getParameter("query");
-    String limit = request.getParameter("numResults");
-    String radius = request.getParameter("radius");
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+                // TODO Auto-generated method stub
 
-    // contains restaurant JSON string results
-    String restaurantJSONstring = YelpRestaurantService.getRestaurantInfo(term,limit, radius);
-				
-    response.getWriter().append("Served at: ").append(request.getContextPath());
-  }
+                // Get queries
+                String term = request.getParameter("query");
+                String limit = request.getParameter("numResults");
+                String radius = request.getParameter("radius");
 
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-   */
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    doGet(request, response);
-  }
+                // contains restaurant JSON string results
+                String restaurantJSONstring = YelpRestaurantService.getRestaurantInfo(term, limit, radius);
+
+                response.setContentType("application/json");
+                response.getWriter().append(restaurantJSONstring).flush();
+        }
+
+        /**
+         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                doGet(request, response);
+        }
 
 }
--- backend/src/test/java/ImHungryTest/BackendTest.java
+++ backend/src/test/java/ImHungryTest/BackendTest.java
@@ -1,37 +1,46 @@
 package ImHungryTest;
 
-public class BackendTest {
-	
-	// @Test
-	// public void testYelpRestaurntQuery() throws UnsupportedEncodingException {
-	// 	YelpRestaurantService yrs = new YelpRestaurantService();
-	// 	assertTrue( yrs.getRestaurantInfo("Chinese", "5", "10").contains("Northern Cafe"));
-	// 	assertTrue( yrs.getRestaurantInfo("Chinese", "5", "10").contains("Los Angeles"));
-				
-	// }
-	
-	// @Test(expected=RuntimeException.class)
-	// public void testYelpRestaurntEmptyQuery() throws UnsupportedEncodingException {
-	// 	YelpRestaurantService yrs = new YelpRestaurantService();
-	// 	String json = yrs.getRestaurantInfo("", "", "");
-	// } 
-	
-	// @Test
-	// public void testRecipeMalformed() throws UnirestException{
-	// 	RecipeServlet rs = new RecipeServlet(); 
-	// 	assertTrue(rs.getRecipeData("asdfasdfasdf", "10").contains("number"));
-	// }
+import java.io.UnsupportedEncodingException;
 
-	// @Test
-	// public void testRecipeQuery() throws UnirestException{
-		
-	// 	RecipeServlet rs = new RecipeServlet();
-        // assertTrue( rs.getRecipeData("soup", "3").contains("Red Lentil Soup"));
-        // assertTrue( rs.getRecipeData("soup", "3").contains("preparationMinutes"));
-        // assertTrue( rs.getRecipeData("soup", "3").contains("cookingMinutes"));
-        // assertTrue( rs.getRecipeData("soup", "3").contains("image"));
-        // assertTrue( rs.getRecipeData("soup", "3").contains("analyzedInstructions"));
-	// }
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.junit.Test;
+
+import ImHungryServlet.RecipeServlet;
+import ImHungryServlet.YelpRestaurantService;
+import junit.framework.Assert;
+
+public class BackendTest {
+
+        // @Test
+        // public void testYelpRestaurntQuery() throws UnsupportedEncodingException {
+        //         YelpRestaurantService yrs = new YelpRestaurantService();
+        //         Assert.assertTrue(yrs.getRestaurantInfo("Chinese", "5", "10").contains("Northern Cafe"));
+        //         Assert.assertTrue(yrs.getRestaurantInfo("Chinese", "5", "10").contains("Los Angeles"));
+
+        // }
+
+        // @Test(expected = RuntimeException.class)
+        // public void testYelpRestaurntEmptyQuery() throws UnsupportedEncodingException {
+        //         YelpRestaurantService yrs = new YelpRestaurantService();
+        //         String json = yrs.getRestaurantInfo("", "", "");
+        // }
+
+        // @Test
+        // public void testRecipeMalformed() throws UnirestException {
+        //         RecipeServlet rs = new RecipeServlet();
+        //         Assert.assertTrue(rs.getRecipeData("asdfasdfasdf", "10").contains("number"));
+        // }
+
+        // @Test
+        // public void testRecipeQuery() throws UnirestException {
+
+        //         RecipeServlet rs = new RecipeServlet();
+        //         Assert.assertTrue(rs.getRecipeData("soup", "3").contains("Red Lentil Soup"));
+        //         Assert.assertTrue(rs.getRecipeData("soup", "3").contains("preparationMinutes"));
+        //         Assert.assertTrue(rs.getRecipeData("soup", "3").contains("cookingMinutes"));
+        //         Assert.assertTrue(rs.getRecipeData("soup", "3").contains("image"));
+        //         Assert.assertTrue(rs.getRecipeData("soup", "3").contains("analyzedInstructions"));
+        // }
 
 }
-
