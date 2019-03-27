1fd5ca06970e45cde4365fff882af36d6e598050
Joshua Min <minje@usc.edu>
2019-03-27 10:39:46 -0700

    Starting on List Management Page styling and impl

--- frontend/src/components/ListManagementPage.tsx
+++ frontend/src/components/ListManagementPage.tsx
@@ -1,40 +1,62 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { withRouter } from 'react-router-dom';
-import { RouteComponentProps } from 'react-router';
-// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import {
+  Card,
+  CardContent,
+  Grid,
+  Theme,
+  createStyles,
+  withStyles,
+} from '@material-ui/core';
 
-const ListManagementPage: React.FC<RouteComponentProps> = props => {
+import Header from './sub-components/Header';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: '0 8px',
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-start',
+    },
+    main: {
+      marginTop: 10,
+    },
+    card: {
+      marginBottom: 10,
+    },
+    container: {
+      marginLeft: '5%',
+      marginRight: '5%',
+    },
+  });
+
+const ListManagementPage: React.FC<any> = props => {
   const [list, setList] = useState([1, 3, 5, 7, 9]);
+  const { classes } = props;
 
   return (
-    <div>
-      <h1>{props.location.pathname.slice(7)}</h1>
-      {/* <DragDropContext>
-          <Droppable droppableId="droppable">
-          {(provided, snapshot) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
-          {list.map((item, idx) => (
-          <Draggable key={item} draggableId={item.toString()} index={idx}>
-          {(provided, snapshot) => (
-          <div
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          >
-          {item}
-          </div>
-          )}
-          </Draggable>
-          ))}
-          {provided.placeholder}
-          </div>
-          )}
-          </Droppable>
-	  </DragDropContext> */}
-      {/* asdjasd*/}
+    <div className={classes.root}>
+      <Header />
+      <div className={classes.container}>
+        <h1>{props.location.pathname.slice(7)}</h1>
+        <Grid container spacing={16} className={classes.main}>
+          <Grid item xs={12}>
+            {list.map((item, idx) => (
+              <Card key={idx} className={classes.card}>
+                <CardContent>{item}</CardContent>
+              </Card>
+            ))}
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 };
 
-export default withRouter(ListManagementPage);
+export default withStyles(styles)(withRouter(ListManagementPage));
