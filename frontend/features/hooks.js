c195c0783774a2f8beb5b70a0b372e40de32a7eb
Joshua Min <minje@usc.edu>
2019-03-31 11:35:40 -0700

    list management tests

---
+++ frontend/features/hooks.js
@@ -0,0 +1,25 @@
+const { After, Before, AfterAll } = require('cucumber');
+const scope = require('./support/scope');
+
+Before(async () => {
+  // clean up database models, etc. here
+});
+
+After(async () => {
+  if (scope.browser && scope.context.currentPage) {
+    const cookies = await scope.context.currentPage.cookies();
+    if (cookies && cookies.length > 0) {
+      await scope.context.currentPage.deleteCookie(...cookies);
+    }
+    // close the web page down
+    await scope.context.currentPage.close();
+    // wipe the context's currentPage value
+    scope.context.currentPage = null;
+  }
+});
+
+AfterAll(async () => {
+  if (scope.browser) {
+    await scope.browser.close();
+  }
+});
