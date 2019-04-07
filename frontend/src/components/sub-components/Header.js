c924e2f53af7c707ab91a6806171fd5f7c83bd75
Joshua Min <minje@usc.edu>
2019-04-07 14:57:00 -0700

    Frontend firebase logout integration

--- frontend/src/components/sub-components/Header.js
+++ frontend/src/components/sub-components/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import { withStyles } from "@material-ui/core/styles";
-import PropTypes from "prop-types";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -12,7 +12,14 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import { ListItemText, FormControl, InputLabel, Select, MenuItem, Input } from "@material-ui/core";
+import {
+  ListItemText,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Input,
+} from '@material-ui/core';
 
 const styles = theme => ({
   root: {
@@ -20,86 +27,107 @@ const styles = theme => ({
   },
   button: {
     position: 'absolute',
-    right: 20
-  }
+    right: 20,
+  },
 });
 
 class Header extends React.Component {
   state = {
     open: false,
-    list: ''
-  }
+    list: '',
+  };
+
   handleDrawerOpen = () => {
-      this.setState({
-          open: true
-      })
-  }
-  handleDrawerClose = () => {
-      this.setState({
-          open: false
-      })
-  }
-  handleChange = (e) => {
     this.setState({
-      list: e.target.value
-    })
-  }
+      open: true,
+    });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      list: e.target.value,
+    });
+  };
+
   handleList = () => {
-    this.props.history.push('/lists/'+this.state.list);
-  }
+    this.props.history.push('/lists/' + this.state.list);
+  };
+
   handleBack = () => {
     this.props.history.push('/');
-  }
+  };
+
+  handleLogout = () => {};
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
         <AppBar position="static">
-            <ToolBar>
-                <Typography variant="h6" color="inherit">
-                    I'm Hungry
-                </Typography>
-                <IconButton color="inherit" className={classes.button} onClick={this.handleDrawerOpen} id="menuBtn">
-                    <MenuIcon />
-                </IconButton>
-            </ToolBar>
+          <ToolBar>
+            <Typography variant="h6" color="inherit">
+              I'm Hungry
+            </Typography>
+            <IconButton
+              color="inherit"
+              className={classes.button}
+              onClick={this.handleDrawerOpen}
+              id="menuBtn"
+            >
+              <MenuIcon />
+            </IconButton>
+          </ToolBar>
         </AppBar>
         <Drawer variant="persistent" anchor="right" open={this.state.open}>
-            <div>
-                <IconButton onClick={this.handleDrawerClose}>
-                    <ChevronRightIcon />
-                </IconButton>
-                <Divider />
-                <List>
-                    <ListItem>
-                       <FormControl>
-                        <InputLabel>List</InputLabel>
-                        <Select
-                          value={this.state.list}
-                          onChange={this.handleChange}
-                          input={<Input name="list" id="list" />}
-                          id="dropdown"
-                        >
-                          <MenuItem id="" value="">
-                            <em>None</em>
-                          </MenuItem>
-                          <MenuItem id="Favorites" value={'Favorites'}>Favorites</MenuItem>
-                          <MenuItem id="ToExplore" value={'ToExplore'}>To Explore</MenuItem>
-                          <MenuItem id="DoNotSow" value={'DoNotShow'}>Do Not Show</MenuItem>                    
-                        </Select>
-                       </FormControl>
-                    </ListItem>
-                    <ListItem button>
-                      <ListItemText primary="Manage List" onClick={this.handleList}/>
-                    </ListItem>
-                    <ListItem button>
-                      <ListItemText primary="Return to Search" onClick={this.handleBack}/>
-                    </ListItem>
-                    <ListItem button>
-                      <ListItemText primary="Log Out" />
-                    </ListItem>
-                </List>
-            </div>
+          <div>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronRightIcon />
+            </IconButton>
+            <Divider />
+            <List>
+              <ListItem>
+                <FormControl>
+                  <InputLabel>List</InputLabel>
+                  <Select
+                    value={this.state.list}
+                    onChange={this.handleChange}
+                    input={<Input name="list" id="list" />}
+                    id="dropdown"
+                  >
+                    <MenuItem id="" value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem id="Favorites" value={'Favorites'}>
+                      Favorites
+                    </MenuItem>
+                    <MenuItem id="ToExplore" value={'To Explore'}>
+                      To Explore
+                    </MenuItem>
+                    <MenuItem id="DoNotSow" value={'Do Not Show'}>
+                      Do Not Show
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Manage List" onClick={this.handleList} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText
+                  primary="Return to Search"
+                  onClick={this.handleBack}
+                />
+              </ListItem>
+              <ListItem button onClick={this.handleLogout}>
+                <ListItemText primary="Log Out" />
+              </ListItem>
+            </List>
+          </div>
         </Drawer>
       </div>
     );
@@ -107,6 +135,6 @@ class Header extends React.Component {
 }
 
 Header.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
 export default withStyles(styles)(withRouter(Header));
